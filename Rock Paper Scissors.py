e3f0efec10a06edfd1c6d1fab579c3a00ed310b8
Manu Varma <varmam@bxscience.edu>
2017-07-28 23:00:15 -0400

    Basic game of RPS

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask_ask import Ask, statement, question
+import random
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@app.route('/')
+def homepage():
+    return "Hello"
+
+@ask.launch
+def start_skill():
+    msg = "Rock, paper or scissors?"
+    return question(msg)
+
+def get_comp_answer():
+    a = random.random()
+    if a < 1.0/3:
+        return "Rock"
+    elif a < 2.0/3:
+        return "Paper"
+    else:
+        return "Scissors"
+
+
+@ask.intent("RockIntent")
+def rock_intent():
+    comp = get_comp_answer()
+    if comp == "Rock":
+        return statement("Tie")
+    elif comp == "Scissors":
+        return statement("You Win")
+    else:
+        return statement("You lose")
+
+
+@ask.intent("PaperIntent")
+def paper_intent():
+    comp = get_comp_answer()
+    if comp == "Paper":
+        return statement("Tie")
+    elif comp == "Rock":
+        return statement("You Win")
+    else:
+        return statement("You lose")
+
+
+@ask.intent("ScissorsIntent")
+def scissors_intent():
+    comp = get_comp_answer()
+    if comp == "Scissors":
+        return statement("Tie")
+    elif comp == "Paper":
+        return statement("You Win")
+    else:
+        return statement("You lose")
+
+if __name__ == '__main__':
+    app.run(debug=True)
