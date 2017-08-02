070c70b474c63c54d3b0dde85ebeb79525ddc7af
Henry Bosch <henry@dhcp-18-111-4-248.dyn.MIT.EDU>
2017-08-02 13:53:18 -0400

    recreated userdata files that were accidentally deleted.

---
+++ userdata/userdata.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+__all__ = ["Userdata"]
+
+class Userdata:
+    def __init__:
+        self.user_history = {}
+
+        self.usernicknames = {}
+
+        self.user_lists = defaultdict(list())
